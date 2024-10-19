26c80826c91e59d465cf7bd6758d2d343262924e
Annisa Qurrotaa'yun <annisaayun23@gmail.com>
2024-10-19 17:00:31 +0700

    sidebar

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('login');
 });
 Route::get('/dashboard', function () {
     return view('dashboard');
+});
+Route::get('/super_admin/dataguru', function () {
+    return view('/super_admin/dataguru');
+});
+Route::get('/app', function () {
+    return view('/app');
 });
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
@@ -16,3 +23,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
